3f0b54755c1355b28d53463bc2c571ad44bb2374
Ihor Leonov <ihorleonow@gmail.com>
2024-02-22 16:57:26 +0200

    ADD | companies page

---
+++ src/app/companies/[...id]/page.tsx
@@ -0,0 +1,21 @@
+import Header from '@/app/components/header';
+import React from 'react';
+
+interface PageProps {
+  params: { id: string[] };
+}
+
+// export function generateStaticParams() {
+//   return [{ id: '1' }, { id: '2' }, { id: '3' }];
+// }
+
+export default function Page({ params }: PageProps) {
+  console.log(params.id);
+
+  return (
+    <>
+      <Header>Companies ({String(params.id)})</Header>
+      <p>{new Date().toTimeString()}</p>
+    </>
+  );
+}
